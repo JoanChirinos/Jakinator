56597c26b0b4ba530274b17c5d34e99e0e5d9073
JoanChirinos <jchirinos@stuy.edu>
2018-05-19 16:28:17 -0400

    .

--- go.py
+++ go.py
@@ -19,36 +19,46 @@ def go():
 
     #toAdd = [fs.getvalue("newCharacter"), fs.getvalue("question"), fs.getValue("oldCharacter")]
 
-##    straw = open("qa.txt", "rU")
-##    oldFile = straw.read()
-##    straw.close()
+    straw = open("qa.txt", "rU")
+    oldFile = straw.read()
+    straw.close()
 
     goingToAdd = "\n" + toAdd[1] + "\n" + toAdd[2] + "\n" + toAdd[0]
 
     print "<html><body><p>" + goingToAdd + "</p></body></html>"
 
     
-    
 
-##    oldFile += goingToAdd
-##
-##    straw = open("qa.txt", "w+")
-##    straw.write(oldFile)
-##    straw.close()
-##    print """
-##                <html>
-##                    
-##                    <head>
-##                        <title>College Trip Playlist</title>
-##                        <script type="text/javascript">
-##                            window.location.replace("thanks.html");
-##                        </script>
-##                        <link href="style.css" type="text/css" rel="stylesheet">
-##                    </head>
-##                    
-##                    <body>
-##                    </body>
-##                    
-##                </html>"""
+    oldFile = [i.split(",") for i in oldFile.split("\n")]
+
+    d = {}
+    for i in oldFile:
+        d[i[0]] = i[1]
+
+    for x in toAdd:
+        t = x.split(",")
+        d[t[0]] = t[1]
+
+    newFile = [i + ',' + d[i] for i in d.keys()]
+    newFile = ','.join(newFile)
+
+    straw = open("qa.txt", "w+")
+    straw.write(newFile)
+    straw.close()
+    print """
+                <html>
+                    
+                    <head>
+                        <title>College Trip Playlist</title>
+                        <script type="text/javascript">
+                            window.location.replace("thanks.html");
+                        </script>
+                        <link href="style.css" type="text/css" rel="stylesheet">
+                    </head>
+                    
+                    <body>
+                    </body>
+                    
+                </html>"""
 
 go()
