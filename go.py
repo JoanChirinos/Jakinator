c83fa4e586b4641f1699fd9b55a89326091ee1b3
JoanChirinos <jchirinos@stuy.edu>
2018-05-19 16:13:30 -0400

    .

--- go.py
+++ go.py
@@ -8,15 +8,20 @@ print 'Content-type: text/html\n\n'
 
 def go():
     fs = cgi.FieldStorage()
-    toAdd = [fs.getvalue("newCharacter", "help"), fs.getvalue("question", "help pls"), fs.getValue("oldCharacter", "help pl0x")]
+    #toAdd = [fs.getvalue("newCharacter"), fs.getvalue("question"), fs.getValue("oldCharacter")]
 ##
 ##    straw = open("qa.txt", "rU")
 ##    oldFile = straw.read()
 ##    straw.close()
 
-    goingToAdd = "\n" + toAdd[1] + "\n" + toAdd[2] + "\n" + toAdd[0]
+    #goingToAdd = "\n" + toAdd[1] + "\n" + toAdd[2] + "\n" + toAdd[0]
 
-    print "<html><body><p>" + goingToAdd + "</p></body></html>"
+    #print "<html><body><p>" + goingToAdd + "</p></body></html>"
+
+    print "<html><body>"
+    for i in fs.keys():
+        print "<p>" + i + "</p>"
+    print "</body></html>"
     
 ##
 ##    oldFile += goingToAdd
